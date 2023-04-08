593d105ddc0b0691289d327f17e44193296b7041
Drew Immerman <drewimmerman@gmail.com>
2023-04-08 16:08:20 -0400

    Update driver-test setup

--- test/spec/mysql5.spec.ts
+++ test/spec/mysql5.spec.ts
@@ -1,11 +1,11 @@
 import 'jasmine';
-import { MySql5Driver } from '../../src';
+import { DatabaseMySql5 } from '../../src';
 import { connectionOptionsMySql5 } from '../connection-options';
 import { test } from 'riao-driver-test/src';
 
 test({
 	name: 'MySQL 5',
-	driver: MySql5Driver,
+	db: DatabaseMySql5,
 	expectedVersion: /^5\.7\.[0-9]+$/,
 	connectionOptions: connectionOptionsMySql5,
 });
