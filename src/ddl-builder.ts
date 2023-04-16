b657089e8c744d0f8f33af98415eb479fe4fe231
Drew Immerman <drewimmerman@gmail.com>
2023-04-16 12:12:59 -0400

    Add change column support

--- src/ddl-builder.ts
+++ src/ddl-builder.ts
@@ -1,6 +1,12 @@
 import { DataDefinitionBuilder } from 'riao-dbal/src';
 
 export class MySqlDataDefinitionBuilder extends DataDefinitionBuilder {
+	public alterColumnStatement(column: string): this {
+		this.sql += 'CHANGE COLUMN ' + column + ' ';
+
+		return this;
+	}
+
 	public createUserPassword(password: string): this {
 		this.sql += 'IDENTIFIED BY "' + password + '" ';
 
